237f7b2aed06f2992caeeb466a5ad9997c486169
liammaricato <liammaricato@gmail.com>
2019-11-22 20:45:49 -0300

    :tada: First files

---
+++ TestaVariaveis/TestaVariaveis.java
@@ -0,0 +1,11 @@
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("olá novo teste!");
+
+        int idade;
+        idade = 21;
+
+        System.out.println(idade);
+        System.out.println("A minha idade é " + idade);
+    }
+}
