0f7844e9e771d87dd6888610f6456339f551478a
liammaricato <liammaricato@gmail.com>
2019-11-26 00:30:50 -0300

    Exemplo de if e else

---
+++ TestaCondicional/TestaCondicional.java
@@ -0,0 +1,12 @@
+public class TestaCondicional {
+    public static void main(String[] args) {
+        int idade = 21;
+        if (idade >= 18) {
+            System.out.println("Você já é maior de idade!");
+            System.out.println("Seja bem-vindo!");
+        }
+        else {
+            System.out.println("Infelizmente você não pode entrar! :(");
+        }
+    }
+}
